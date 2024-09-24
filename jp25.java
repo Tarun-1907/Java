b94c2ad06699a35d425cb2dfe42070550522942a
Tarun <tarunsaha200319@gmail.com>
2024-09-24 13:28:46 +0530

    Initial

---
+++ jp25.java
@@ -0,0 +1,14 @@
+public class jp25 {
+    public static void main(String args[]){
+        int a=65;
+        int b=66;
+        int c=67;
+        int d=68;
+        char ascia=(char)a;
+        char ascib=(char)b;
+        char ascic=(char)c;
+        char ascid=(char)d;
+        System.out.println(ascia+"\n"+ascib+"\n"+ascic+"\n"+ascid);
+        
+    }
+}
